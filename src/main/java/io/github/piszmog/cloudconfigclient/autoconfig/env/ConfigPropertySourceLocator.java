328b5a488a58d5bbace0b9e86f33a68b64edad68
Piszmog <piszmogcloud@gmail.com>
2018-07-23 20:23:27 -0600

    update docs

--- src/main/java/io/github/piszmog/cloudconfigclient/autoconfig/env/ConfigPropertySourceLocator.java
+++ src/main/java/io/github/piszmog/cloudconfigclient/autoconfig/env/ConfigPropertySourceLocator.java
@@ -20,7 +20,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 /**
- * JSON property source locator. Locates the JSON files to be loaded as property sources.
+ * Property source locator. Locates the JSON files to be loaded as property sources.
  * <p>
  * Created by Piszmog on 7/21/2018
  */
