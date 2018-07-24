8e17dad1a3c1f36d089fdde3f0b9cdb5c0b2c280
Piszmog <piszmogcloud@gmail.com>
2018-07-24 08:22:53 -0600

    update for Spring Boot 2/Spring Framework 5

--- src/main/java/io/github/piszmog/cloudconfigclient/autoconfig/template/OAuth2ConfigTemplateAutoConfiguration.java
+++ src/main/java/io/github/piszmog/cloudconfigclient/autoconfig/template/OAuth2ConfigTemplateAutoConfiguration.java
@@ -2,12 +2,15 @@ package io.github.piszmog.cloudconfigclient.autoconfig.template;
 
 import io.github.piszmog.cloudconfig.template.ConfigTemplate;
 import io.github.piszmog.cloudconfig.template.impl.OAuth2ConfigTemplate;
+import io.pivotal.spring.cloud.service.config.ConfigClientOAuth2BootstrapConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.config.client.ConfigClientProperties;
+import org.springframework.cloud.config.client.ConfigServiceBootstrapConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 
 /**
@@ -18,6 +21,7 @@ import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResour
 @Configuration
 @ConditionalOnClass( OAuth2ProtectedResourceDetails.class )
 @ConditionalOnProperty( value = "spring.cloud.config.client.oauth2.clientId" )
+@Import( { ConfigServiceBootstrapConfiguration.class, ConfigClientOAuth2BootstrapConfiguration.class } )
 public class OAuth2ConfigTemplateAutoConfiguration
 {
     // ============================================================
