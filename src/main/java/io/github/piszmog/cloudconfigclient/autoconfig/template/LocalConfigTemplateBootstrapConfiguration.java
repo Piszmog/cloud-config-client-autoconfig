cbafb7a19bb1ff3edf80f973f3d27dc8c1f9e604
Piszmog <piszmogcloud@gmail.com>
2018-07-25 21:21:55 -0600

    refactor bootstraps class names and loading order and flatten maps if read from json or yml

--- src/main/java/io/github/piszmog/cloudconfigclient/autoconfig/template/LocalConfigTemplateBootstrapConfiguration.java
+++ src/main/java/io/github/piszmog/cloudconfigclient/autoconfig/template/LocalConfigTemplateBootstrapConfiguration.java
@@ -2,7 +2,6 @@ package io.github.piszmog.cloudconfigclient.autoconfig.template;
 
 import io.github.piszmog.cloudconfig.template.ConfigTemplate;
 import io.github.piszmog.cloudconfig.template.impl.LocalConfigTemplate;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.config.client.ConfigClientProperties;
@@ -10,17 +9,18 @@ import org.springframework.cloud.config.client.ConfigServiceBootstrapConfigurati
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.annotation.Order;
 
 /**
  * Auto-configuration for creating a {@link LocalConfigTemplate}.
  * <p>
  * Created by Piszmog on 5/5/2018
  */
+@Order
 @Configuration
-@ConditionalOnBean( ConfigClientProperties.class )
-@ConditionalOnProperty( value = "spring.cloud.config.client.oauth2.clientId", matchIfMissing = true )
+@ConditionalOnProperty( value = "spring.cloud.config.client.oauth2.client-id", matchIfMissing = true )
 @Import( ConfigServiceBootstrapConfiguration.class )
-public class LocalConfigTemplateAutoConfiguration
+public class LocalConfigTemplateBootstrapConfiguration
 {
     // ============================================================
     // Beans:
@@ -32,6 +32,7 @@ public class LocalConfigTemplateAutoConfiguration
      * @param configClientProperties the config client properties.
      * @return The config template.
      */
+    @Order
     @Bean
     @ConditionalOnMissingBean
     public ConfigTemplate localConfigTemplate( final ConfigClientProperties configClientProperties )
