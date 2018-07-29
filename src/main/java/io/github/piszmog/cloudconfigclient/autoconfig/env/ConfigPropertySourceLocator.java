4cf856ab93b2416b90846317717e3e86b92e3074
Piszmog <piszmogcloud@gmail.com>
2018-07-29 14:11:08 -0600

    use Java 8 completable futures to load files -- attempt to increase performance

--- src/main/java/io/github/piszmog/cloudconfigclient/autoconfig/env/ConfigPropertySourceLocator.java
+++ src/main/java/io/github/piszmog/cloudconfigclient/autoconfig/env/ConfigPropertySourceLocator.java
@@ -8,6 +8,7 @@ import io.github.piszmog.cloudconfig.client.impl.FileConfigClient;
 import io.github.piszmog.cloudconfigclient.autoconfig.env.model.Resource;
 import io.github.piszmog.cloudconfigclient.autoconfig.env.support.MapFlattener;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.concurrent.BasicThreadFactory;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.core.env.CompositePropertySource;
@@ -18,10 +19,12 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.stream.Collectors;
 
 /**
  * Property source locator. Locates the JSON files to be loaded as property sources.
@@ -37,6 +40,7 @@ public class ConfigPropertySourceLocator
     // ============================================================
 
     private static final String PROPERTY_SOURCE_NAME = "jsonResource";
+    private static final String THREAD_NAME = "ConfigLocator";
     private static final ObjectMapper YAML_MAPPER = new ObjectMapper( new YAMLFactory() );
     private static final JavaPropsMapper PROPERTIES_MAPPER = new JavaPropsMapper();
 
@@ -45,7 +49,11 @@ public class ConfigPropertySourceLocator
     // ============================================================
 
     private final FileConfigClient fileConfigClient;
-    private final ExecutorService executorService = Executors.newFixedThreadPool( 10 );
+    private final ExecutorService executorService = Executors.newFixedThreadPool( 10,
+            new BasicThreadFactory.Builder().namingPattern( THREAD_NAME + "-%d" )
+                    .daemon( true )
+                    .priority( Thread.MAX_PRIORITY )
+                    .build() );
 
     // ============================================================
     // Constructors:
@@ -73,24 +81,26 @@ public class ConfigPropertySourceLocator
      */
     public PropertySource<?> locateConfigResources( final List<Resource> resources )
     {
-        List<Future<?>> futures = new ArrayList<>();
         CompositePropertySource composite = new CompositePropertySource( PROPERTY_SOURCE_NAME );
-        for ( Resource resource : resources )
-        {
-            final String directoryPath = resource.getDirectory();
-            final List<String> files = resource.getFiles();
-            getJsonConfiguration( composite, directoryPath, files, futures );
-        }
+        //
+        // Load files asynchronously
+        //
+        List<CompletableFuture<Void>> futures = new ArrayList<>();
+        resources.stream().filter( Objects::nonNull ).map( resource ->
+                getJsonConfiguration( composite, resource.getDirectory(), resource.getFiles() ) ).forEach( futures::addAll );
+        final CompletableFuture<List<Void>> listCompletableFuture = CompletableFuture.allOf( futures.toArray( new CompletableFuture[ 0 ] ) )
+                .thenApply( future -> futures.stream().map( CompletableFuture::join ).collect( Collectors.toList() ) );
         try
         {
-            for ( Future<?> future : futures )
-            {
-                future.get();
-            }
+            //
+            // Wait for files to complete -- don't want Spring to start setting up Beans without all property sources
+            //
+            listCompletableFuture.get();
+            logger.info( "Successfully loaded all external configuration files." );
         }
         catch ( InterruptedException | ExecutionException e )
         {
-            throw new ConfigResourceException( "Failed to retrieve file.", e );
+            throw new ConfigResourceException( "Failed to retrieve files.", e );
         }
         finally
         {
@@ -103,16 +113,21 @@ public class ConfigPropertySourceLocator
     // Private Methods:
     // ============================================================
 
-    private void getJsonConfiguration( final CompositePropertySource composite, final String directoryPath, final List<String> files, final List<Future<?>> futures )
+    private List<CompletableFuture<Void>> getJsonConfiguration( final CompositePropertySource composite,
+                                                                final String directoryPath,
+                                                                final List<String> files )
     {
-        for ( String fileName : files )
-        {
-            futures.add( executorService.submit( () -> loadConfigurationFile( composite, directoryPath, fileName ) ) );
-        }
+        return files.stream().filter( Objects::nonNull ).map( file -> CompletableFuture.supplyAsync( () ->
+                loadConfigurationFile( directoryPath, file ), executorService ).thenAccept( mapPropertySource -> {
+            if ( mapPropertySource != null )
+            {
+                composite.addPropertySource( mapPropertySource );
+            }
+        } ) ).collect( Collectors.toList() );
     }
 
     @SuppressWarnings( "unchecked" )
-    private void loadConfigurationFile( final CompositePropertySource composite, final String directoryPath, final String fileName )
+    private MapPropertySource loadConfigurationFile( final String directoryPath, final String fileName )
     {
         final String filePath = getFilePath( directoryPath, fileName );
         Map file;
@@ -121,11 +136,11 @@ public class ConfigPropertySourceLocator
             logger.info( "Loading configuration " + filePath + "..." );
             if ( StringUtils.endsWithIgnoreCase( fileName, ".yml" ) || StringUtils.endsWithIgnoreCase( fileName, ".yaml" ) )
             {
-                file = getYAMLFile( directoryPath, fileName, filePath );
+                file = getYAMLFile( directoryPath, fileName );
             }
             else if ( StringUtils.endsWithIgnoreCase( fileName, ".properties" ) )
             {
-                file = getPropertiesFile( directoryPath, fileName, filePath );
+                file = getPropertiesFile( directoryPath, fileName );
             }
             else
             {
@@ -139,8 +154,9 @@ public class ConfigPropertySourceLocator
         if ( !file.isEmpty() )
         {
             final Map flattenMap = MapFlattener.flatten( file );
-            composite.addPropertySource( new MapPropertySource( filePath, flattenMap ) );
+            return new MapPropertySource( filePath, flattenMap );
         }
+        return null;
     }
 
     private String getFilePath( final String directoryPath, final String fileName )
@@ -148,31 +164,31 @@ public class ConfigPropertySourceLocator
         return directoryPath + "/" + fileName;
     }
 
-    private Map getYAMLFile( final String directoryPath, final String fileName, final String filePath ) throws ConfigException
+    private Map getYAMLFile( final String directoryPath, final String fileName ) throws ConfigException
     {
         final Map file;
-        final String yamlFile = fileConfigClient.getFileFromDefaultBranch( fileName, directoryPath, String.class );
+        final byte[] yamlFile = fileConfigClient.getFileFromDefaultBranch( fileName, directoryPath, byte[].class );
         try
         {
             file = YAML_MAPPER.readValue( yamlFile, Map.class );
         }
         catch ( IOException e )
         {
-            throw new ConfigResourceException( "Failed to convert " + filePath + " from YAML format to be loaded in the property sources.", e );
+            throw new ConfigResourceException( "Failed to convert " + fileName + " from YAML format to be loaded in the property sources.", e );
         }
         return file;
     }
 
-    private Map getPropertiesFile( final String directoryPath, final String fileName, final String filePath ) throws ConfigException
+    private Map getPropertiesFile( final String directoryPath, final String fileName ) throws ConfigException
     {
         final Map file;
         try
         {
-            file = PROPERTIES_MAPPER.readValue( fileConfigClient.getFileFromDefaultBranch( fileName, directoryPath, String.class ), Map.class );
+            file = PROPERTIES_MAPPER.readValue( fileConfigClient.getFileFromDefaultBranch( fileName, directoryPath, byte[].class ), Map.class );
         }
         catch ( IOException e )
         {
-            throw new ConfigResourceException( "Failed to convert " + filePath + " from PROPERTIES format to be loaded in the property sources.", e );
+            throw new ConfigResourceException( "Failed to convert " + fileName + " from PROPERTIES format to be loaded in the property sources.", e );
         }
         return file;
     }
